0245de8ba0b9b3f4d26cca9f0b4b90be6a12b5e1
israelidl12 <israelidl12@gmail.com>
2024-03-05 16:44:43 -0600

    Creación de función de encriptar

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const matriz_code= [
     ["o","ober"],
     ["u","ufat"]
 ];
+let textoaencriptar;
 
-function textoAEncriptar(){
-    let textoaencriptar = document.getElementById("textEntrada").value;
+
+function encriptar(textoaencriptar){
+    textoaencriptar=document.getElementById("textEntrada").value;
+    for(let i=0; i<matriz_code.length;i++){
+        if(textoaencriptar.includes(matriz_code[i][0])){
+            textoaencriptar=textoaencriptar.replaceAll(
+                matriz_code[i][0],
+                matriz_code[i][1]
+            );
+        }
+    }
     console.log(textoaencriptar);
-    return textoaencriptar;
 }
